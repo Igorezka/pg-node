5fd1f3bf94b7c1773505242000f443a3f542e1ce
Igor Popov <65647498+Igorezka@users.noreply.github.com>
2026-04-02 21:16:55 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     ufw allow $port
 fi
 
+# nano /etc/ufw/before.rules
+# icmp INPUT раздел
+# -A ufw-before-input -p icmp --icmp-type source-quench -j DROP
+
 sudo bash -c "$(curl -sL https://github.com/PasarGuard/scripts/raw/main/pg-node.sh)" @ install
