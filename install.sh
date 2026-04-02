03246ecce73bfd8d7b863b3a8589038d2e44f86b
Igor Popov <65647498+Igorezka@users.noreply.github.com>
2026-04-02 21:24:47 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ufw enable && ufw allow OpenSSH
 
 read -p "Открыть доп. порт? [y/n]: " -n 1 -r
+echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Введите номер порта:"
@@ -15,3 +16,4 @@ fi
 # -A ufw-before-input -p icmp --icmp-type source-quench -j DROP
 
 sudo bash -c "$(curl -sL https://github.com/PasarGuard/scripts/raw/main/pg-node.sh)" @ install
+
